263c401ca41c69ab9561e42087b0d36eee2c3b1f
Baptiste <baptiste@matierenoire.io>
2019-12-15 23:21:27 +0100

    start project

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
-import 'bootstrap';
-import $ from "jquery";
+import 'bootstrap'
+import $ from 'jquery'
 
-import imageURL from './assets/trollHuntersArcadia.png';
+import imageURL from './assets/trollHuntersArcadia.png'
 
 // JQuery
 $(document).ready(() => {
-
-    // bootstrap tooltips
-    $(() => {
-        $('[data-toggle="tooltip"]').tooltip({ trigger: "hover" })
-    });
-
-});
+  // bootstrap tooltips
+  $(() => {
+    $('[data-toggle="tooltip"]').tooltip({ trigger: 'hover' })
+  })
+})
