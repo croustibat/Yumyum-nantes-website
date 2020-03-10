ce529f22efae104a77cf7ad3604ce28b7c31fcc4
Baptiste <baptiste@matierenoire.io>
2020-03-10 14:49:27 +0100

    clean up

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
-import 'bootstrap'
 import $ from 'jquery'
-
-import imageURL from './assets/trollHuntersArcadia.png'
 
 // JQuery
 $(document).ready(() => {
-  // bootstrap tooltips
-  $(() => {
-    $('[data-toggle="tooltip"]').tooltip({ trigger: 'hover' })
-  })
+  $(() => {})
 })
